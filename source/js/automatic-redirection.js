20f8fb3d85befaf9575aad0bab0895b7d0a264a5
dong4j <dong4j@gmail.com>
2025-01-05 15:12:19 +0800

    局域网不跳转

--- source/js/automatic-redirection.js
+++ source/js/automatic-redirection.js
@@ -1,11 +1,19 @@
-var hostname1 = 'dong4j' + '.' + 'github' + '.' + 'io'
-var hostname2 = 'blog' + '.' + 'dong4j' + '.ink:1024'
-var hostname3 = 'blog' + '.' + 'dong4j' + '.ink'
-var hostname4 = 'localhost'
-var hostnameNow = document.location.hostname
-if (hostnameNow !== hostname1 && hostnameNow !== hostname2 && hostnameNow !== hostname3 && hostnameNow !== hostname4) {
-	var sourceDomain = 'https://' + hostname1
-	window.location.href = sourceDomain + document.location.pathname
+var hostname1 = 'dong4j' + '.' + 'github' + '.' + 'io';
+var hostname2 = 'blog' + '.' + 'dong4j' + '.ink:1024';
+var hostname3 = 'blog' + '.' + 'dong4j' + '.ink';
+var hostname4 = 'localhost';
+var hostnameNow = document.location.hostname;
+var localNetworkPrefix1 = '192.168.21.';
+var localNetworkPrefix2 = '192.168.31.';
+
+// 检查当前主机名是否为本地局域网地址
+function isLocalNetwork(hostname) {
+  return hostname.startsWith(localNetworkPrefix1) || hostname.startsWith(localNetworkPrefix2);
+}
+
+if (hostnameNow !== hostname1 && hostnameNow !== hostname2 && hostnameNow !== hostname3 && hostnameNow !== hostname4 && !isLocalNetwork(hostnameNow)) {
+  var sourceDomain = 'https://' + hostname1;
+  window.location.href = sourceDomain + document.location.pathname;
 }
 
 // https://blog.meta-code.top/2024/10/27/2024-15/
