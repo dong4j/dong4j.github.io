f9d490757077559a34dfa84d02876977b823c10f
dong4j <dong4j@gmail.com>
2025-01-05 11:43:41 +0800

    更新个人页面

--- source/js/custom.js
+++ source/js/custom.js
@@ -1,16 +1,20 @@
 // 分享本页
 function share() {
-    let url = window.location.origin + window.location.pathname
-    new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
-    anzhiyu.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+  let url = window.location.origin + window.location.pathname;
+  new ClipboardJS(".share", {
+    text: function () {
+      return "标题：" + document.title + "\n链接：" + url;
+    },
+  });
+  anzhiyu.snackbarShow("本页链接已复制到剪切板，快去分享吧~");
 }
 
 // hexo-blog-decrypt 解密之后重新刷新页面才能显示图片
 function handleHexoBlogDecryptEvent() {
-    // 刷新页面
-    window.location.reload();
-  }
-  
+  // 刷新页面
+  window.location.reload();
+}
+
 // 添加事件监听器
-window.addEventListener('hexo-blog-decrypt', handleHexoBlogDecryptEvent);
-  
+window.addEventListener("hexo-blog-decrypt", handleHexoBlogDecryptEvent);
+
